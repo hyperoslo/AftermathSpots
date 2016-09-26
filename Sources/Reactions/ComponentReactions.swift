9991690e69711a7461797431b2bfac056023b3d3
Christoffer Winterkvist <christoffer@hyper.no>
2016-09-26 12:27:03 +0200

    Use regular dispatch instead of Sugar delay

--- Sources/Reactions/ComponentReactions.swift
+++ Sources/Reactions/ComponentReactions.swift
@@ -14,7 +14,12 @@ public struct ComponentReloadBuilder: ReactionBuilder {
 
   private func stopReloading() {
     if self.controller?.refreshControl.refreshing == true {
-      delay(0.1) { self.controller?.refreshControl.endRefreshing() }
+      dispatch_after(
+        dispatch_time(DISPATCH_TIME_NOW, Int64(0.1 * Double(NSEC_PER_SEC))),
+        dispatch_get_main_queue()
+      ) {
+        self.controller?.refreshControl.endRefreshing()
+      }
     }
   }
 
